c3c2a5cb795c8af4789cf9aed0d25f5ebf53f0a2
pedro.smiranda <pedro.smiranda@goias.gov.br>
2025-09-10 15:57:09 -0300

    Implement HTTP client to fetch and display country data from REST API

--- src/main/java/edu/sistemasdistribuidos/paises/Main.java
+++ src/main/java/edu/sistemasdistribuidos/paises/Main.java
@@ -1,7 +1,28 @@
 package edu.sistemasdistribuidos.paises;
 
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import edu.sistemasdistribuidos.paises.models.Pais;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://restcountries.com/v3.1/all?fields=area,borders,continents,demonym,name,languages,population,region,subregion,translations")).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() == 200) {
+            Gson gson = new Gson();
+            Type paisListType = new TypeToken<List<Pais>>(){}.getType();
+            List<Pais> paises = gson.fromJson(response.body(), paisListType);
+            paises.forEach(pais -> System.out.println(pais.getName().getCommon()));
+        }
     }
 }
